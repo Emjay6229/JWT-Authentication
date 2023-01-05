ebad8f8e4ad7d8b49bd88ca4eccd89700d10b8fa
Joshua Onwuemene <emjay6299@gmail.com>
2023-01-05 21:41:47 +0100

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -22,28 +22,3 @@ const verifyToken = (req, res, next) => {
 
 module.exports = { verifyToken };
 
-// exports.authenticateUser = (req, res, next) => {
-//   // check if there is an authorization token
-//   if (!req.headers.authorization) {
-//     return res.status(401).json({
-//       message: "Authentication is required",
-//     });
-//   }
-//   let headerSplit = req.headers.authorization;
-
-//   if(headerSplit[0] !== "Bearer") {
-//     return res.status(401).json({
-//         message: "authorization format is Bearer <token>"
-//     })
-//   }
-//   let token = headerSplit[1];
-// //   check validity
-//   jwt.verify(token, SECRET, (err, decodedToken) => {
-//     if (err) return res.status(500).json({ err })
-//     if (!decodedToken) {
-//         return res.status(401).json({message: "invalid authorization token, login to continue"})
-//     }
-//     // allow user to continue
-//     next()
-//   })
-// };
