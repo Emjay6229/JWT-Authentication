b109066786501d78756b721379944b849eddb043
Joshua Onwuemene <emjay6299@gmail.com>
2023-01-05 21:53:08 +0100

    Update authRoute.js

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,8 +2,6 @@ const router = require("express").Router();
 const { verifyToken } = require("../middlewares/auth.js");
 const authController = require("../controllers/authController.js");
 const pageController = require("../controllers/pageController.js");
-const uploadController = require("../controllers/uploadController")
-// const chunkController = require("../controllers/chunkController.js");
 
 //Set up routes for registration and Login 
 router
@@ -17,6 +15,6 @@ router
   .post("/login", authController.loginUser)
   .put("/signup/:id", authController.updateUser)
   .delete("/signup/:id", authController.deleteUser)
-  // .post('/uploads', upload.single('file'), uploadController.uploadFile)
-    
+  
 module.exports = router;
+
