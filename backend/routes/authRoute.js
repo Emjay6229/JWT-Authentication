9878106c1356d52b0218dfbed5325f8194ffffc1
Joshua Onwuemene <emjay6299@gmail.com>
2023-01-06 09:06:12 +0100

    Update authRoute.js

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -8,13 +8,13 @@ router
   .get("/", pageController.getLandingPage)
   .get("/signup", pageController.getSignUpPage)
   .get("/login", pageController.getLoginPage)
-  .get("/:id", authController.getSingleUser)
+  .get("/welcome/:id", authController.getSingleUser)
   .get("/welcome", verifyToken, pageController.getWelcomePage)
   .get("/library", verifyToken, pageController.getLibraryPage)
   .post("/signup", authController.registerNewUser)
   .post("/login", authController.loginUser)
-  .put("/:id", authController.updateUser)
-  .delete("/:id", authController.deleteUser)
+  .put("/welcome/:id", verifyToken, authController.updateUser)
+  .delete("/welcome/:id", verifyToken, authController.deleteUser)
   
 module.exports = router;
 
